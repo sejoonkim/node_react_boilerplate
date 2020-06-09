fc23fe828de2e4d164e2c1fa50e6c79f107d7d9e
sejoonkim <sejoondev@gmail.com>
2020-06-10 06:24:40 +0900

    feat: Login

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"); // get express module
 const app = express(); // create express app
 const port = 5000;
 const bodyParser = require("body-parser");
-
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 const { User } = require("./models/User");
@@ -11,6 +11,8 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json type
 app.use(bodyParser.json());
+app.use(cookieParser());
+
 const mongoose = require("mongoose");
 mongoose
   .connect(config.mongoURI, {
@@ -31,6 +33,33 @@ app.post("/register", (req, res) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      }
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        // save Token: cookie, local storage
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
     });
   });
 });
