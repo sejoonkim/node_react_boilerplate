cf03d7af7b66c3bd2a80c961d09192ed77040487
sejoonkim <sejoondev@gmail.com>
2020-06-10 05:46:27 +0900

    feat: env Variables

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express(); // create express app
 const port = 5000;
 const bodyParser = require("body-parser");
 
+const config = require("./config/key");
+
 const { User } = require("./models/User");
 
 // get data with format application/x-www/form-urlencoded
@@ -11,19 +13,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://sejoonkim:Mongodb_94@boilerplate-1qdid.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!")); // root directory
+app.get("/", (req, res) => res.send("Hello World")); // root directory
 
 app.post("/register", (req, res) => {
   // get info needed for register -> save to DB
