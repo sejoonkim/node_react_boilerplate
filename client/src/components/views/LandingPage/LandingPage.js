6a0d76d583997d88eeda4fafabce7c56a280043c
sejoonkim <sejoondev@gmail.com>
2020-06-10 10:17:32 +0900

    feat: Logout Function

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get(`/api/users/logout`).then((response) => {
+      if (response.data.success) {
+        props.history.push("/login");
+      } else {
+        alert("Failed to logout.");
+      }
+    });
+  };
   return (
     <div
       style={{
@@ -15,7 +25,8 @@ function LandingPage() {
         height: "100vh",
       }}
     >
-      Landing Page
+      <h2>Landing Page</h2>
+      <button onClick={onClickHandler}>Log out</button>
     </div>
   );
 }
